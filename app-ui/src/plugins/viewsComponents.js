d2505d6b3ab211ff1d32029ad085d6f0e90f5105
andresmauro17 <andresmauro17@gmail.com>
2021-05-23 19:39:02 -0500

    first approach dashboard layout

--- app-ui/src/plugins/viewsComponents.js
+++ app-ui/src/plugins/viewsComponents.js
@@ -1,8 +1,10 @@
 import TestingComponent from "../views/testingComponent.vue";
+import DashboardLayout from "../views/Layout/DashboardLayout.vue";
 
 const testingComponent = {
   install(app) {
     app.component("testing-component", TestingComponent);
+    app.component("dashboard-layout", DashboardLayout);
   },
 };
 
---
+++ app-ui/src/views/Layout/DashboardLayout.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper">
+    <div class="main-content">
+      <dashboard-navbar :type="'light'"></dashboard-navbar>
+
+      <div @click="$sidebar.displaySidebar(false)">
+        <slot name="content"></slot>
+      </div>
+      <!-- <content-footer v-if="!$route.meta.hideFooter"></content-footer> -->
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-new */
+import PerfectScrollbar from "perfect-scrollbar";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+
+function hasElement(className) {
+  return document.getElementsByClassName(className).length > 0;
+}
+
+function initScrollbar(className) {
+  if (hasElement(className)) {
+    new PerfectScrollbar(`.${className}`);
+  } else {
+    // try to init it later in case this component is loaded async
+    setTimeout(() => {
+      initScrollbar(className);
+    }, 100);
+  }
+}
+
+import DashboardNavbar from "./DashboardNavbar.vue";
+// import ContentFooter from "./ContentFooter.vue";
+
+export default {
+  components: {
+    DashboardNavbar,
+    // ContentFooter,
+  },
+  methods: {
+    initScrollbar() {
+      let isWindows = navigator.platform.startsWith("Win");
+      if (isWindows) {
+        initScrollbar("sidenav");
+      }
+    },
+  },
+  mounted() {
+    this.initScrollbar();
+  },
+};
+</script>
+<style lang="scss"></style>
