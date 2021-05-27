71d8bf47a87dd8b7919f5f94e2b1abdc6c44f387
andresmauro17 <andresmauro17@gmail.com>
2021-05-26 20:36:35 -0500

    sensor model

--- apps/sensors/admin.py
+++ apps/sensors/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+# django imports
+from django.contrib import admin
+
+# local import
+from apps.sensors.models import Sensor
+
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'manufacturer_id', 'device_type']
+    list_editable = ['name','manufacturer_id', 'device_type']
+    list_filter = ['device_type']
+    search_fields = []
+    ordering = []
--- apps/sensors/models.py
+++ apps/sensors/models.py
@@ -1,3 +1,23 @@
+# Django
 from django.db import models
 
-# Create your models here.
+from apps.utils.models import AppModel
+
+class Sensor(models.Model):
+    """ Sensor Model.
+        This model is the sensors that take the data
+
+    """
+    name = models.CharField(max_length=100)
+
+    VARIABLE_CHOICES = (
+        ('TEMPERATURE', 'Temperatura'),
+        ('HUMIDITY','Humedad'),
+    )
+    device_type = models.CharField(max_length=100, choices=VARIABLE_CHOICES)
+
+    # most brands will have some sort of id you'll want to track
+    manufacturer_id = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return '{}'.format(self.id)
