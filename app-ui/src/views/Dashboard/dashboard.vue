d52b072a7530332d2047069ab1c7380f1b6a0f73
Andres Cuervo <andresmauro17@gmail.com>
2022-03-18 16:46:59 -0500

    indicator

--- app-ui/src/views/Dashboard/dashboard.vue
+++ app-ui/src/views/Dashboard/dashboard.vue
@@ -1,13 +1,46 @@
 <template>
   <div>
-    <h1>hola dashboard</h1>
+    <base-header class="pb-6" type="">
+      <div class="row align-items-center py-4">
+        <div class="col-lg-6 col-7">
+          <h6 class="h2 d-inline-block mb-0">Gthux</h6>
+          <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+            <ol class="breadcrumb breadcrumb-links">
+              <li class="breadcrumb-item">
+                <router-link to="/"><i class="fas fa-home"></i></router-link>
+              </li>
+              <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+              <li class="breadcrumb-item active" aria-current="page">
+                Indicadores
+              </li>
+            </ol>
+          </nav>
+        </div>
+        <div class="col-lg-6 col-5 text-right">
+          <base-button size="sm" type="neutral">New</base-button>
+          <base-button size="sm" type="neutral">Filters</base-button>
+        </div>
+      </div>
+      
+    </base-header>
+    <div class="container-fluid mt--6">
+      <div class="row">
+          <div class="col-xl-4">
+            <indicator />
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Indicator from "@/components/indicator/Indicator.vue";
 export default {
   mounted() {
     console.log("dashboard mounted");
+  },
+  components: {
+    Indicator,
   },
 };
 </script>
