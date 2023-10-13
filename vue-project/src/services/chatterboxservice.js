ee4edce24606b3ebff54f04c1ed16924ab5e0802
Andres Cuervo <andresmauro17@gmail.com>
2023-10-13 15:49:20 -0500

    storing data

--- vue-project/src/services/chatterboxservice.js
+++ vue-project/src/services/chatterboxservice.js
@@ -16,7 +16,7 @@ ChatterBoxService.startMqttClient = async function () {
     username: "",
     password:""
   }
-  const subscribeTopic = "sensors/FF3C0E001705/currentdata/"
+  
   const connectUrl = `ws://${options.host}:${options.port}${options.endpoint}`
   let client = {}
   try {
@@ -24,40 +24,7 @@ ChatterBoxService.startMqttClient = async function () {
   } catch (error) {
     console.log(error);
   }
-
-  //MQTT CONNECTION SUCCESS
-  client.on("connect", () => {
-    console.log(client);
-
-    console.log("Connection succeeded!");
-
-    //SDATA SUBSCRIBE
-    client.subscribe(subscribeTopic, { qos: 0 }, err => {
-      if (err) {
-        console.log("Error in DeviceSubscription");
-        return;
-      }
-      console.log("Device subscription Success");
-      console.log(subscribeTopic);
-    });
-
-  });
-
-  client.on("error", error => {
-    console.log("Connection failed", error);
-  });
-
-  client.on("reconnect", error => {
-    console.log("reconnecting:", error);
-    // this.getMqttCredentialsForReconnection();
-  });
-
-  client.on("message", (topic, message) => {
-    console.log("Message from topic " + topic + " -> ");
-    console.log(message.toString());
-  });
-
-  return "";
+  return client;
 };
 
 
--- vue-project/src/stores/chatterbox.js
+++ vue-project/src/stores/chatterbox.js
@@ -2,11 +2,46 @@ import { defineStore } from 'pinia'
 import ChatterBoxService from "@/services/chatterboxservice.js"
 export default defineStore('chatterbox', {
   state: () => ({
+    client: {},
+    messages:[],
   }),
   actions:{
-    startMqttClient(){
+    async startMqttClient(){
       console.log("startMqttClient")
-      ChatterBoxService.startMqttClient();
+
+
+      this.client = await ChatterBoxService.startMqttClient();
+      console.log("client this",this.client)
+      const subscribeTopic = "sensors/FF3C0E001705/currentdata/"
+
+      //MQTT CONNECTION SUCCESS
+      this.client.on("connect", () => {
+        console.log("Connection succeeded!");
+
+        //SDATA SUBSCRIBE
+        this.client.subscribe(subscribeTopic, { qos: 0 }, err => {
+          if (err) {
+            console.log("Error in DeviceSubscription");
+            return;
+          }
+          console.log("Device subscription Success");
+          console.log(subscribeTopic);
+        });
+
+      });
+
+      this.client.on("error", error => {
+        console.log("Connection failed", error);
+      });
+
+      this.client.on("reconnect", error => {
+        console.log("reconnecting:", error);
+      });
+
+      this.client.on("message", (topic, message) => {
+        console.log("Message from topic " + topic + " -> ");
+        this.messages.push(JSON.parse(message.data))
+      });
     },
   }
 })
