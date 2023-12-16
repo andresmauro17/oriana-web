25f37396eee5677e29d3e5f21858159889af89ce
Andres Cuervo <andresmauro17@gmail.com>
2023-12-16 13:25:18 -0500

    fix env variables for vite and django

--- vue-project/src/services/chatterboxservice.js
+++ vue-project/src/services/chatterboxservice.js
@@ -1,20 +1,29 @@
 
 import * as mqtt from 'mqtt/dist/mqtt.min'
+const HOST = import.meta.env.VITE_WEBSOCKET_HOST
+const PORT = import.meta.env.VITE_WEBSOCKET_PORT
+const USERNAME = import.meta.env.VITE_WEBSOCKET_USERNAME
+const PASSWORD = import.meta.env.VITE_WEBSOCKET_PASSWORD
 const ChatterBoxService = {};
 
 ChatterBoxService.startMqttClient = async function () {
-  // console.log("ChatterBoxService.startMqttClient")
+  console.log("ChatterBoxService.startMqttClientttt")
+  console.log("HOST:", HOST)
+  console.log("PORT:", PORT)
+  console.log("USERNAME:", USERNAME)
+  console.log("PASSWORD", PASSWORD)
+  console.log("---------------------------------")
   const options ={
-    host:"localhost",
-    port:8083,
+    host:HOST,
+    port:PORT,
     endpoint:"/mqtt",
     clean:false,
     connectTimeout: 5000,
     reconnectPeriod:5000,
 
     clientId: `web_andresmauro17_${Math.floor(Math.random() * 1000000 + 1)}`,
-    username: "",
-    password:""
+    username: USERNAME,
+    password: PASSWORD
   }
   
   const connectUrl = `ws://${options.host}:${options.port}${options.endpoint}`
