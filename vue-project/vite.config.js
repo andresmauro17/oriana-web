a893e9a9403f6b80554aa9f592a87a77484cde07
Andres Cuervo <andresmauro17@gmail.com>
2023-12-19 21:09:02 -0500

    fixing build env

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir:"../",
+  // envDir:"../",
   plugins: [vue()],
   resolve: {
     alias: {
