11e0491d9d6841de4486f3a913b50cd5a665141d
Andres Cuervo <andresmauro17@gmail.com>
2024-08-03 13:11:44 -0500

    adding tls

--- config/settings/base.py
+++ config/settings/base.py
@@ -139,7 +139,7 @@ EMAIL_HOST = os.getenv('APP_EMAIL_HOST')
 EMAIL_HOST_USER = os.getenv('APP_EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('APP_EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.getenv('APP_EMAIL_PORT')
-# EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
 # EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
 
 # Internationalization
